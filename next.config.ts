2f9ed1a26cf70290cdab518e541a614814f9e14d
Ângelo Quintarelli Greenhalgh <angelogreenhalgh@hotmail.com>
2026-03-09 17:58:08 -0300

    :hammer: Salvando novo artigo para o post - configurando exibição de imagens no post

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,24 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     domains: ['localhost', 'example.com'], // adicione todos os domínios necessários aqui
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '3000',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'http',
+        hostname: 'example.com',
+        port: '3000',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+    // Em desenvolvimento, você pode desativar a otimização
+    unoptimized: process.env.NODE_ENV === 'development',
   },
 };
 
--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,10 +1,15 @@
 'use server';
 
+import { drizzleDb } from '@/db/drizzle';
+import { postTable } from '@/db/drizzle/schemas';
 import { makePartialPublicPost, PublicPost } from '@/dto/post/dto';
 import { PostCreateSchema } from '@/lib/post/validations';
 import { PostModel } from '@/models/post/post-model';
 import { getZodErrorMessages } from '@/utils/get-zod-error-messages';
 import { makeSlugFromText } from '@/utils/make-slug-from-text';
+import { revalidateTag } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 import { v4 as uuidV4 } from 'uuid';
 
 type CreatePostActionState = {
@@ -47,8 +52,8 @@ export async function createPostAction(
     slug: makeSlugFromText(validPostData.title),
   };
 
-  return {
-    formState: newPost,
-    errors: [],
-  };
+  await drizzleDb.insert(postTable).values(newPost);
+
+  revalidateTag('posts', 'seconds');
+  redirect(`/admin/post/${newPost.id}`);
 }
