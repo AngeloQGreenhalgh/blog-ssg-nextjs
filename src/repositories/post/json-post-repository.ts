124d8d18775bf4c703de7213a8c71bddc4f4deba
Ângelo Quintarelli Greenhalgh <angelogreenhalgh@hotmail.com>
2026-03-12 08:29:01 -0300

    :hammer: Ajustando o antigo repositório

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,15 +1,11 @@
+import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { resolve } from 'path';
-import { writeFile, readFile, access } from 'fs/promises';
-import { constants as fsConstants } from 'fs';
-import { v4 as uuidv4 } from 'uuid';
-import { PostModel } from '@/models/post/post-model';
-import { SIMULATE_WAIT_IN_MS } from '@/lib/constants';
+import { readFile, writeFile } from 'fs/promises';
 
-// Definindo o caminho raiz da aplicação
+const simulateWaitMs = Number(process.env.SIMULATE_WAIT_IN_MS) || 0;
+
 const ROOT_DIR = process.cwd();
-
-// Caminho para o arquivo posts.json
 const JSON_POSTS_FILE_PATH = resolve(
   ROOT_DIR,
   'src',
@@ -20,129 +16,110 @@ const JSON_POSTS_FILE_PATH = resolve(
 
 export class JsonPostRepository implements PostRepository {
   private async simulateWait() {
-    if (SIMULATE_WAIT_IN_MS <= 0) return;
+    if (simulateWaitMs <= 0) return;
 
-    await new Promise(resolve => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+    await new Promise(resolve => setTimeout(resolve, simulateWaitMs));
   }
 
-  // Função para ler os posts do arquivo JSON
-  private async readFromDisk() {
+  private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
     const parsedJson = JSON.parse(jsonContent);
     const { posts } = parsedJson;
     return posts;
   }
 
-  // Função para escrever os posts no arquivo JSON
-  private async writeToDisk(posts: PostModel[]) {
-    await writeFile(
-      JSON_POSTS_FILE_PATH,
-      JSON.stringify({ posts }, null, 2),
-      'utf-8',
-    );
-  }
-
-  private async init() {
-    try {
-      await access(JSON_POSTS_FILE_PATH, fsConstants.F_OK);
-      // Arquivo existe, não faz nada
-    } catch {
-      // Arquivo não existe, cria um novo arquivo em branco (com array de posts)
-      await this.writeToDisk([]);
-    }
+  private async writeToDisk(posts: PostModel[]): Promise<void> {
+    const jsonToString = JSON.stringify({ posts }, null, 2);
+    await writeFile(JSON_POSTS_FILE_PATH, jsonToString, 'utf-8');
   }
 
   async findAllPublic(): Promise<PostModel[]> {
-    await this.simulateWait(); // simulando espera
-    await this.init();
-
-    console.log('\n', 'findAllPublic', '\n');
+    await this.simulateWait();
 
     const posts = await this.readFromDisk();
-
-    return posts.filter((post: PostModel) => post.published) as PostModel[];
+    return posts.filter(post => post.published);
   }
 
   async findAll(): Promise<PostModel[]> {
-    await this.simulateWait(); // simulando espera
-    await this.init();
+    await this.simulateWait();
 
     const posts = await this.readFromDisk();
-
-    return posts as PostModel[];
+    return posts;
   }
 
   async findById(id: string): Promise<PostModel> {
-    await this.simulateWait(); // simulando espera
-    await this.init();
     const posts = await this.findAllPublic();
-    const post = posts.find((post: PostModel) => post.id === id);
+    const post = posts.find(post => post.id === id);
 
-    if (!post) {
-      throw new Error('Post não encontrado para ID');
-    }
+    if (!post) throw new Error('Post não encontrado para ID');
 
-    return post as PostModel;
+    return post;
   }
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
-    //await this.simulateWait(); // simulando espera
-    await this.init();
     const posts = await this.findAllPublic();
-    const post = posts.find((post: PostModel) => post.slug === slug);
+    const post = posts.find(post => post.slug === slug);
 
-    if (!post) {
-      throw new Error('Post não encontrado para slug');
-    }
+    if (!post) throw new Error('Post não encontrado para slug');
 
-    return post as PostModel;
+    return post;
   }
 
-  async create(postData: PostModel): Promise<PostModel> {
-    await this.init();
-    const posts = await this.findAllPublic();
-    postData.id = uuidv4(); // Gera um novo ID único para o post
-    posts.push(postData);
+  async create(post: PostModel): Promise<PostModel> {
+    const posts = await this.findAll();
 
-    // Atualiza o arquivo JSON com o novo array de posts
+    if (!post.id || !post.slug) {
+      throw new Error('Post sem ID ou Slug');
+    }
+
+    const idOrSlugExist = posts.find(
+      savedPost => savedPost.id === post.id || savedPost.slug === post.slug,
+    );
+
+    if (idOrSlugExist) {
+      throw new Error('ID ou Slug devem ser únicos');
+    }
+
+    posts.push(post);
     await this.writeToDisk(posts);
 
-    return postData;
+    return post;
   }
 
-  async update(id: string, postData: PostModel): Promise<PostModel | null> {
-    await this.init();
-    const posts = await this.findAllPublic();
-    const postIndex = posts.findIndex((post: PostModel) => post.id === id);
+  async delete(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const postIndex = posts.findIndex(p => p.id === id);
 
-    if (postIndex === -1) {
-      return null; // Post não encontrado
+    if (postIndex < 0) {
+      throw new Error('Post não existe');
     }
 
-    // Atualiza os dados do post existente
-    posts[postIndex] = { ...posts[postIndex], ...postData };
-
-    // Atualiza o arquivo JSON com o array de posts modificado
-    await this.writeToDisk(posts);
-
-    return posts[postIndex];
-  }
-
-  async delete(id: string): Promise<boolean> {
-    await this.init();
-    const posts = await this.findAllPublic();
-    const postIndex = posts.findIndex((post: PostModel) => post.id === id);
-
-    if (postIndex === -1) {
-      return false; // Post não encontrado
-    }
-
-    // Remove o post do array
+    const post = posts[postIndex];
     posts.splice(postIndex, 1);
-
-    // Atualiza o arquivo JSON com o array de posts modificado
     await this.writeToDisk(posts);
 
-    return true; // Post removido com sucesso
+    return post;
+  }
+
+  async update(
+    id: string,
+    newPostData: Omit<PostModel, 'id' | 'slug' | 'createdAt' | 'updatedAt'>,
+  ): Promise<PostModel> {
+    const posts = await this.findAll();
+    const postIndex = posts.findIndex(p => p.id === id);
+    const savedPost = posts[postIndex];
+
+    if (postIndex < 0) {
+      throw new Error('Post não existe');
+    }
+
+    const newPost = {
+      ...savedPost,
+      ...newPostData,
+      updatedAt: new Date().toISOString(),
+    };
+    posts[postIndex] = newPost;
+    await this.writeToDisk(posts);
+    return newPost;
   }
 }
