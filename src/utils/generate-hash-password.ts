93b2f649a80f6d3de505d717fdefcde0e6f8b4eb
Angelo Quintarelli Greenhalgh <angelogreenhalgh@hotmail.com>
2026-04-15 22:14:22 -0300

    :hammer: Subir o ajuste da senha, não pode ficar na repo

--- src/utils/generate-hash-password.ts
+++ src/utils/generate-hash-password.ts
@@ -1,7 +1,7 @@
 import { hashPassword } from '@/lib/login/manage-login';
 
 (async () => {
-  const minhaSenha = '123456'; // NÃO ESQUECER DE APAGAR SUA SENHA DAQUI
+  const minhaSenha = ''; // NÃO ESQUECER DE APAGAR SUA SENHA DAQUI
   const hasheDaSuaSenhaEmBase64 = await hashPassword(minhaSenha);
   console.log({ hasheDaSuaSenhaEmBase64 });
 })();
